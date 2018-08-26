1160a4120c12f507383dad8ea5f2b0a0caa1d1d9
BuildTools <unconfigured@null.spigotmc.org>
2018-08-26 00:34:43 -0400

    Final version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,76 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
-    }
-}
+
+
+
+        int guess = 50;
+System.out.println("Enter your secret number");
+        int yourNumber = 0;
+        Scanner keyboard = new Scanner(System.in);
+        yourNumber = keyboard.nextInt();
+        //user inputs their number
+
+        String lower = "lower";
+        String higher = "higher";
+
+
+        while (true) {
+
+
+            System.out.println("My guess is " + guess + ". Is my guess higher or lower than your number?");
+
+            String highOrLow = "";
+
+            highOrLow = keyboard.next();
+            //user inputs if guess is higher/lower
+
+
+            if (highOrLow.equals(lower)) {
+
+                int previousGuess = guess;
+                System.out.println("Ok, I will guess a lower number.");
+
+
+                    guess = (int) (Math.random() * 100 + 1);
+
+                    if (guess >= previousGuess){
+                        while(guess >= previousGuess){
+                            guess = (int) (Math.random() * 100 + 1);
+                        }
+                    }
+
+                if (guess == yourNumber){
+                    System.out.println("I guessed your number!");
+                    System.exit(0);
+                }
+
+
+            } else if (highOrLow.equals(higher)) {
+
+                int previousGuess = guess;
+                System.out.println("Ok, I will guess a higher number.");
+
+
+                guess = (int) (Math.random() * 100 + 1);
+
+                if (guess <= previousGuess){
+                    while(guess <= previousGuess){
+                        guess = (int) (Math.random() * 100 + 1);
+                    }
+                }
+
+                if (guess == yourNumber){
+                    System.out.println("I guessed your number!");
+                    System.exit(0);
+                }
+
+
+            } else {
+                System.out.println("Sorry, I do not know what you mean.");
+            }
+
+        }
+    }}
